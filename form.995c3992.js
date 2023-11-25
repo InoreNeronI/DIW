efcff62150278efd890cf8c922b4cfe8b03c7cab
InoreNeronI <martinmozos@gmail.com>
2023-11-25 08:13:35 +0000

    Deploying to gh-pages from @ InoreNeronI/DIW@5e0f00084f2b65be702f0b1a3b5e0b8c6132dcdf 🚀

---
+++ form.995c3992.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){t.target.classList.contains("hamburger-toggle")&&t.target.children[0].classList.toggle("active")});
+//# sourceMappingURL=form.995c3992.js.map
